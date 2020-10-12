71678cf8e0a2c5236511b7c10b12b322c2533a2f
jweiss <jweiss@centeredgesoftware.com>
2020-10-12 16:05:05 -0400

    Load the reports where they would actually load.

--- MobileReporting/MobileReporting/BusinessLogic/Models/ReportMetaData.cs
+++ MobileReporting/MobileReporting/BusinessLogic/Models/ReportMetaData.cs
@@ -9,5 +9,6 @@ namespace MobileReporting.BusinessLogic.Models
         public string Name { get; set; }
         public Guid ReportId { get; set; }
         public string EmbedUrl { get; set; }
+        public string EmbedHtml { get; set; }
     }
 }
--- MobileReporting/MobileReporting/BusinessLogic/Services/ReportService.cs
+++ MobileReporting/MobileReporting/BusinessLogic/Services/ReportService.cs
@@ -63,21 +63,17 @@ namespace MobileReporting.BusinessLogic.Services
             {
                 new ReportMetaData()
                 {
-                    Name = "Report 1",
+                    Name = "Sales",
                     EmbedUrl = "http://www.google.com/",
-                    ReportId = new Guid("71fe3aea-7881-4911-a7a4-9d94b48acfef")
+                    ReportId = new Guid("71fe3aea-7881-4911-a7a4-9d94b48acfef"),
+                    EmbedHtml = @"<iframe width=""100%"" height=""100%"" src =""https://app.powerbi.com/view?r=eyJrIjoiNzIzYzM4ZTAtYzcxMy00ZmE1LTkxYzgtMTEzMDcwOTMyMmIxIiwidCI6IjAwYTNhYzRkLTY5MzUtNDVjYi04Mjk5LWI5ZmU0NGYwMzNmOCIsImMiOjJ9&isMobile=true"" frameborder=""0"" allowFullScreen=""true""></iframe>"
                 },
                 new ReportMetaData()
                 {
-                    Name = "Report 2",
+                    Name = "Labor",
                     EmbedUrl = "http://www.google.com/",
-                    ReportId = Guid.NewGuid()
-                },
-                new ReportMetaData()
-                {
-                    Name = "Report 3",
-                    EmbedUrl = "http://www.google.com/",
-                    ReportId = Guid.NewGuid()
+                    ReportId = Guid.NewGuid(),
+                    EmbedHtml = @"<iframe width=""100%"" height=""100%"" src =""https://app.powerbi.com/view?r=eyJrIjoiZjhmMGRlZjUtYjhlYi00NDhmLWE4NzgtZGE4M2MzYjM4MTZkIiwidCI6IjAwYTNhYzRkLTY5MzUtNDVjYi04Mjk5LWI5ZmU0NGYwMzNmOCIsImMiOjJ9&pageName=ReportSection"" frameborder=""0"" allowFullScreen=""true""></iframe>"
                 }
             };
         }
