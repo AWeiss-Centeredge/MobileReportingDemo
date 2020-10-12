5a062fa9a0f805598f65dfae5226b379b37f7f9e
jweiss <jweiss@centeredgesoftware.com>
2020-10-12 11:24:28 -0400

    A bunch of debug changes

--- MobileReporting/MobileReporting/BusinessLogic/Services/ReportService.cs
+++ MobileReporting/MobileReporting/BusinessLogic/Services/ReportService.cs
@@ -21,7 +21,7 @@ namespace MobileReporting.BusinessLogic.Services
         {
             get
             {
-                var credentials = new TokenCredentials("{flUmzg2aORBZQieLZ9oJo+VJwZHrYSZFBsNPU6lXsrAyVIZdN10Iu1xH76Y1Pl05VzlKQaTUs8XG90vD0FxsvQ==}", "AppKey");
+                var credentials = new TokenCredentials("flUmzg2aORBZQieLZ9oJo+VJwZHrYSZFBsNPU6lXsrAyVIZdN10Iu1xH76Y1Pl05VzlKQaTUs8XG90vD0FxsvQ==", "AppKey");
                 return new PowerBIClient(credentials);
             }
         }
@@ -37,40 +37,49 @@ namespace MobileReporting.BusinessLogic.Services
             {
                 new Guid("71fe3aea-7881-4911-a7a4-9d94b48acfef")
             };
-            using (var client = _powerBiClient)
-            {
-                var reports = await client.Reports.GetReportsWithHttpMessagesAsync();
-                return reports.Body.Value
-                    .Where(report => IDs.Contains(report.Id))
-                    .Select(report => new ReportMetaData()
-                {
-                    Name = report.Name,
-                    EmbedUrl = report.EmbedUrl,
-                    ReportId = report.Id
-                });
-            }
-
-            //return new System.Collections.Generic.List<ReportMetaData>()
+            //try
             //{
-            //    new ReportMetaData()
+            //    using (var client = _powerBiClient)
             //    {
-            //        Name = "Report 1",
-            //        EmbedUrl = "http://www.google.com/",
-            //        ReportId = Guid.NewGuid()
-            //    },
-            //    new ReportMetaData()
-            //    {
-            //        Name = "Report 2",
-            //        EmbedUrl = "http://www.google.com/",
-            //        ReportId = Guid.NewGuid()
-            //    },
-            //    new ReportMetaData()
-            //    {
-            //        Name = "Report 3",
-            //        EmbedUrl = "http://www.google.com/",
-            //        ReportId = Guid.NewGuid()
+            //        var reports = await client.Reports.GetReportsWithHttpMessagesAsync();
+            //        //reports = await client.Reports.get
+            //        return reports.Body.Value
+            //            .Where(report => IDs.Contains(report.Id))
+            //            .Select(report => new ReportMetaData()
+            //            {
+            //                Name = report.Name,
+            //                EmbedUrl = report.EmbedUrl,
+            //                ReportId = report.Id
+            //            });
             //    }
-            //};
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //    throw;
+            //}
+
+            return new System.Collections.Generic.List<ReportMetaData>()
+            {
+                new ReportMetaData()
+                {
+                    Name = "Report 1",
+                    EmbedUrl = "http://www.google.com/",
+                    ReportId = new Guid("71fe3aea-7881-4911-a7a4-9d94b48acfef")
+                },
+                new ReportMetaData()
+                {
+                    Name = "Report 2",
+                    EmbedUrl = "http://www.google.com/",
+                    ReportId = Guid.NewGuid()
+                },
+                new ReportMetaData()
+                {
+                    Name = "Report 3",
+                    EmbedUrl = "http://www.google.com/",
+                    ReportId = Guid.NewGuid()
+                }
+            };
         }
 
         public async Task<Microsoft.PowerBI.Api.Models.Report> GetBiReportAysnc(Guid reportId)
